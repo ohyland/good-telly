5fdbc22d6a157ea28127c764f032d0fa61e84466
ohyland <52823456+ohyland@users.noreply.github.com>
2020-10-13 22:34:35 +0100

    add styles using Material UI, add buttons, separate out into Header, Main and Footer Components

---
+++ src/Header.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Header() {
+  return (
+    <main>
+      <h1
+        style={{
+          fontWeight: 200,
+          fontSize: "3em",
+          textAlign: "center",
+        }}
+      >
+        My React App
+      </h1>
+
+    </main>
+  );
+}
+
+export default Header;
